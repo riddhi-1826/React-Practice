7c2e2ec96bf8b050efb1f681dc6becd4bf621933
riddhi jain <riddhijain1826@gmail.com>
2024-12-13 00:55:30 +0530

    episode-7 completed

---
+++ Episode-7/src/components/Error.js
@@ -0,0 +1,14 @@
+import { useRouteError } from "react-router-dom";
+
+const Error = () => {
+    const err = useRouteError();
+    return(
+        <div>
+            <h1>Oops!!!</h1>
+            <h1>Something Went wrong</h1>
+            <h3>{err.status} : {err.statusText}</h3>
+        </div>
+    )
+}
+
+export default Error;
---
+++ Episode-7/src/utils/constants.js
@@ -0,0 +1,9 @@
+export const CDN_URL = 
+    "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_366/";
+
+export const LOGO_URL = 
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDN051xHnibkrTWV8_-yIxekWpMXfaLFeYPQ&s"
+
+export const SWIGGY_API=
+    "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId="
+    // "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId="
